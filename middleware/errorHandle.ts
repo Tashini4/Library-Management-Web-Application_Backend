768082b8c639645c82492fd5967752dfed9744db
Tashini4 <tashiniwijethunga@gmail.com>
2025-07-21 23:31:52 +0530

    commit

---
+++ middleware/errorHandle.ts
@@ -0,0 +1,24 @@
+import {NextFunction} from "express";
+import * as mongoose from "mongoose";
+import {ApiError} from "../Errors/ApiError";
+
+export const errorHandle = (
+    err:any,
+    req: Request,
+    res: Response,
+    next: NextFunction
+)=> {
+    if (err instanceof mongoose.Error) {
+        res.status(400).json(
+            {
+                message: err.message,
+            }
+        )
+    }
+
+    if (err instanceof ApiError) {
+        res.status(err.statusCode).json({message: err.message});
+        return
+    }
+    res.status(500).json({message: "Internal error"});
+}
---
+++ model/VerificationCodeGenerator.ts
@@ -0,0 +1,4 @@
+export const VerificationCodeGenerator = () =>{
+    const randomSixDigit:number = Math.floor(100000 + Math.random() * 900000);
+    return randomSixDigit;
+}
